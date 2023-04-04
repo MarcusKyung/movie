9355425ff74315885c88b1d8c0c33a489b05e340
MarcusKyung <Kyungmj@gmail.com>
2023-04-04 14:20:27 -0700

    add JS logic for ticket age and movie release pricing

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,36 @@
-function Ticket(time) {
+function Ticket(release, time, age) {
+  this.release = release;
   this.time = time;
+  this.age = age;
+
 }
 
 Ticket.prototype.findPrice = function() {
 let price = 0
-if (this.time === "matinee"){
+if (this.release === "old-movie"){
   price = price + 8;
 } else {
   price = price + 10;
+}
+
+if (this.time === "day"){
+  price = price * 1.5;
+} 
+
+if (this.age === "senior"){
+  price = price - 2;
+} else if (this.age === "child"){
+  price = price -1;
 } return price;
+
 };
 
 function handleFormSubmission(event) {
   event.preventDefault();
   const showtimes = document.querySelector("#showtimes").value;
-  let movieTicket = new Ticket(showtimes);
+  const release = document.querySelector("#movie-option").value;
+  const age = document.querySelector("#age-group").value;
+  let movieTicket = new Ticket(release, showtimes, age);
   movieTicket.findPrice();
   let price = movieTicket.findPrice();
   document.getElementById("ticket-price").innerText = price;
