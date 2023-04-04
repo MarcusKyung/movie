16957ae99b0987697a35c2d9b0aeb824475956e0
MarcusKyung <Kyungmj@gmail.com>
2023-04-04 13:45:51 -0700

    added JS for ticket price with only showtime info

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,27 @@
-function Ticket(movie, time, age) {
-  this.movie = movie;
+function Ticket(time) {
   this.time = time;
-  this.age = age;
 }
 
 Ticket.prototype.findPrice = function() {
 let price = 0
+if (this.time === "matinee"){
+  price = price + 8;
+} else {
+  price = price + 10;
+} return price;
+};
+
+function handleFormSubmission(event) {
+  event.preventDefault();
+  const showtimes = document.querySelector("#showtimes").value;
+  let movieTicket = new Ticket(showtimes);
+  movieTicket.findPrice();
+  let price = movieTicket.findPrice();
+  document.getElementById("ticket-price").innerText = price;
 
 }
+
+window.addEventListener("load", function (){
+  document.querySelector("form#movie-options").addEventListener("submit", handleFormSubmission);
+  
+});
