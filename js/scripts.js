947b1b43fbac511cc1bda3e3c455b886de4fa5ea
MarcusKyung <Kyungmj@gmail.com>
2023-04-04 22:16:10 -0700

    added JS functionality to display movie poster

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,13 @@
-function Ticket(release, time, age) {
+function Ticket(release, time, age, poster) {
   this.release = release;
   this.time = time;
   this.age = age;
-
+  this.poster = poster;
 }
 
 Ticket.prototype.findPrice = function() {
 let price = 0
-if (this.release === "old-movie"){
+if (this.release === "rush" || this.release === "tenet"){
   price = price + 8;
 } else {
   price = price + 10;
@@ -22,22 +22,37 @@ if (this.age === "senior"){
 } else if (this.age === "child"){
   price = price -1;
 } return price;
-
 };
+
+Ticket.prototype.findPoster = function() {
+  if (this.poster === "rush") {
+    imageUrl = "img/rush.jpg";
+    } else if (this.poster === "tenet") {
+      imageUrl = "img/tenet.jpg"; 
+    } else if (this.poster === "john-wick-4") {
+      imageUrl = "img/jw4.jpg";
+    } else if (this.poster === "the-menu") {
+      imageUrl = "img/menu.jpg";
+    } return imageUrl;
+  }; 
+
 
 function handleFormSubmission(event) {
   event.preventDefault();
   const showtimes = document.querySelector("#showtimes").value;
   const release = document.querySelector("#movie-option").value;
   const age = document.querySelector("#age-group").value;
-  let movieTicket = new Ticket(release, showtimes, age);
+  const poster = document.querySelector("#movie-option").value;
+  console.log(poster);
+  let movieTicket = new Ticket(release, showtimes, age, poster);
   movieTicket.findPrice();
   let price = movieTicket.findPrice();
   document.getElementById("ticket-price").innerText = price;
-
+  movieTicket.findPoster();
+  document.getElementById("poster-image").setAttribute("src", imageUrl);
+  document.getElementById("poster-image").setAttribute("width", "300px");
 }
 
 window.addEventListener("load", function (){
   document.querySelector("form#movie-options").addEventListener("submit", handleFormSubmission);
-  
 });
